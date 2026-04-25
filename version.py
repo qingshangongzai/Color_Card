8b21f9a73e5d70190448e9b91b831938d3d07926
青山公仔 <qingshangongzai@163.com>
2026-04-26 01:28:03 +0800

    [内容调整] 更新版本信息

--- version.py
+++ version.py
@@ -11,7 +11,7 @@ class VersionManager:
         self.minor: int = 9
         self.patch: int = 0
         self.build: int = 0
-        self.prerelease: str = " · Beta 4"
+        self.prerelease: str = ""
 
         # 核心版本信息
         self.version: str = f"{self.major}.{self.minor}.{self.patch}{self.prerelease}"
